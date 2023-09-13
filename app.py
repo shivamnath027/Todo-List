6a5a1f42d4cdccce02fe4cfb4743674f68bbcf28
Shivam Nath <shivamnath027@gmail.com>
2023-09-13 20:07:28 +0530

    CODE WITH _ FLASK

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask #import Flask module from flask package
 from flask import render_template #import render_template from flask package
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(200),nullable=False)
+    desc = db.Column(db.String(500),nullable=False)
+    date_created = db.Column(db.DateTime,default= datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
 @app.route('/')
 def hello_world():
     # return '<p>Hello world<p>'
